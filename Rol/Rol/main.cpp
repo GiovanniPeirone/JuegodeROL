0d33399c183a439cec46b83f77ce144156e9a594
maxiiol <134154147+maxiiol@users.noreply.github.com>
2024-09-28 17:44:59 -0300

    polno

--- Rol/Rol/main.cpp
+++ Rol/Rol/main.cpp
@@ -2,9 +2,33 @@
 
 using namespace std;
 
+const int X = 20;
+
+const int Y = 20;
+
+char mesa[X][Y];
+
+char mapear(char mesa[X][Y]) {
+
+	for (int i = 0; i < X * Y; i++) {
+
+		for (int a = 0; a < X ; a++) {
+
+			mesa[i][a] += 254;
+
+
+		}
+			
+
+	}
+	return mesa  ;
+}
 int main() {
+	mesa[X][Y]=mapear(mesa[X][Y]);
 
-	cout << "hola ceo es mi primer dia " << endl;
 
+	
+	
+	
 	return 0;
 }
